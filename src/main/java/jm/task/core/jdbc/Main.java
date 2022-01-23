1d478cc5601d3a3535fd8c1b46273ce5097fa6cd
Anton Mirzaev <Eltor@yandex.ru>
2022-01-23 13:26:00 +0300

    renovated

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,8 +6,8 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-
         UserService userService = new UserServiceImpl();
+
         userService.createUsersTable();
         userService.saveUser("Lora", "Adams", (byte) 33);
         userService.saveUser("Tom", "Jons", (byte) 2);
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -10,6 +17,30 @@ public class Util {
     private static final String password = "root";
     private static final String connectionUrl = "jdbc:mysql://localhost:3306/jdbslesson?autoReconnect=true";
     private static  Connection connection;
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        Configuration configuration = new Configuration();
+
+        Properties property = new Properties();
+        property.setProperty(Environment.URL, connectionUrl);
+        property.setProperty(Environment.USER, userName);
+        property.setProperty(Environment.PASS, password);
+
+        configuration.setProperties(property);
+        configuration.addAnnotatedClass(User.class);
+        StandardServiceRegistryBuilder registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        try {
+            sessionFactory = configuration.buildSessionFactory(registry.build());
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
     
     public static Connection getConnection() {
         if(connection == null){
